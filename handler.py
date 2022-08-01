9a0d629b6083db6fbcd1c512fe9eca12ed8bffc0
ticleless <kyy20802@gmail.com>
2022-08-01 13:58:41 +0900

    Add lambda handler, kinesis putrecord

--- handler.py
+++ handler.py
@@ -1,26 +1,31 @@
 import json
+import boto3
+import uuid
 
-# data = {
-#     "result": "success",
-#     "error_code": "0",
-#     "device_id: "39278391",
-#         "coord": {
-#             "lon": -8.61,
-#             "lat": 41.15
-#           },
-#     "server_time": "1416895635000",
-#         "temperature": 38.5,
-#         "pressure": 1014,
-#         "humidity": 88,
-#         "co2": 413.2,
-# }
+data = {
+    "result": "success",
+    "error_code": "0",
+    "device_id": "39278391",
+        "coord": {
+            "lon": -8.61,
+            "lat": 41.15
+          },
+    "server_time": "1416895635000",
+        "temperature": 38.5,
+        "pressure": 1014,
+        "humidity": 88,
+        "co2": 413.2,
+}
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
+def lambda_handler(event, context):
+    client = boto3.client('kinesis')
+    
+    response = client.put_record(
+        StreamName='test-kyy',
+        Data=json.dumps(event),
+        PartitionKey=str(uuid.uuid4())
+    )
+    
+    
+    print(response)
     return response
