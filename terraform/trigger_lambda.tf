6b52e15f00d251d765f0bb1bb005f5876a92118f
eun07 <eunleong0726@gmail.com>
2022-08-05 17:08:25 +0900

    Fix : APi Gateway Resource to Module

--- terraform/trigger_lambda.tf
+++ terraform/trigger_lambda.tf
@@ -80,82 +80,107 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 
 // API Gateway stuff
 
-resource "aws_apigatewayv2_api" "lambda" {
-  name          = "apigw-http-lambda"
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
+
+  name          = "trigger-lambda-APIGateway"
+  description   = "My awesome HTTP API Gateway"
   protocol_type = "HTTP"
-  description   = "Serverlessland API Gwy HTTP API and AWS Lambda function"
+  create_api_domain_name           = false
 
-  cors_configuration {
-      allow_credentials = false
-      allow_headers     = []
-      allow_methods     = [
-          "GET",
-          "HEAD",
-          "OPTIONS",
-          "POST",
-      ]
-      allow_origins     = [
-          "*",
-      ]
-      expose_headers    = []
-      max_age           = 0
+integrations = {
+  "$default" = {
+    lambda_arn = aws_lambda_function.app.invoke_arn
+    payload_format_version = "2.0"
   }
 }
 
-
-resource "aws_apigatewayv2_stage" "default" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  name        = "$default"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
-  depends_on = [aws_cloudwatch_log_group.api_gw]
 }
-
-resource "aws_apigatewayv2_integration" "app" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  integration_uri    = aws_lambda_function.app.invoke_arn
-  integration_type   = "AWS_PROXY"
-}
-
-resource "aws_apigatewayv2_route" "any" {
-  api_id = aws_apigatewayv2_api.lambda.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.app.id}"
-}
-
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
-
-  retention_in_days = 7
-}
-
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.app.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+  source_arn = "${module.api_gateway.apigatewayv2_api_execution_arn}/*"
 }
+
+# resource "aws_apigatewayv2_api" "lambda" {
+#   name          = "apigw-http-lambda"
+#   protocol_type = "HTTP"
+#   description   = "Serverlessland API Gwy HTTP API and AWS Lambda function"
+
+#   cors_configuration {
+#       allow_credentials = false
+#       allow_headers     = []
+#       allow_methods     = [
+#           "GET",
+#           "HEAD",
+#           "OPTIONS",
+#           "POST",
+#       ]
+#       allow_origins     = [
+#           "*",
+#       ]
+#       expose_headers    = []
+#       max_age           = 0
+#   }
+# }
+
+
+# resource "aws_apigatewayv2_stage" "default" {
+#   api_id = aws_apigatewayv2_api.lambda.id
+
+#   name        = "$default"
+#   auto_deploy = true
+
+#   access_log_settings {
+#     destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+#     format = jsonencode({
+#       requestId               = "$context.requestId"
+#       sourceIp                = "$context.identity.sourceIp"
+#       requestTime             = "$context.requestTime"
+#       protocol                = "$context.protocol"
+#       httpMethod              = "$context.httpMethod"
+#       resourcePath            = "$context.resourcePath"
+#       routeKey                = "$context.routeKey"
+#       status                  = "$context.status"
+#       responseLength          = "$context.responseLength"
+#       integrationErrorMessage = "$context.integrationErrorMessage"
+#       }
+#     )
+#   }
+#   depends_on = [aws_cloudwatch_log_group.api_gw]
+# }
+
+# resource "aws_apigatewayv2_integration" "app" {
+#   api_id = aws_apigatewayv2_api.lambda.id
+
+#   integration_uri    = aws_lambda_function.app.invoke_arn
+#   integration_type   = "AWS_PROXY"
+# }
+
+# resource "aws_apigatewayv2_route" "any" {
+#   api_id = aws_apigatewayv2_api.lambda.id
+#   route_key = "$default"
+#   target    = "integrations/${aws_apigatewayv2_integration.app.id}"
+# }
+
+# resource "aws_cloudwatch_log_group" "api_gw" {
+#   name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+#   retention_in_days = 7
+# }
+
+# resource "aws_lambda_permission" "api_gw" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.app.function_name
+#   principal     = "apigateway.amazonaws.com"
+
+#   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*"
+# }
 
 resource "aws_iam_role_policy_attachment" "terraform_lambda_iam_policy_kinesis_execution" {
   role = "${aws_iam_role.lambda_exec.id}"
